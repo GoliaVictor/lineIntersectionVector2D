cdd3891a9b5c5af4bf0558227646b148d1b121d9
GoliaVictor <62819330+GoliaVictor@users.noreply.github.com>
2022-02-17 09:02:01 +1100

    Basic functionality
    Create lines, checkIntersection, and update function

---
+++ mySketch.js
@@ -0,0 +1,35 @@
+const intersection = new LineIntersectionVector2D()
+let creatingLine;
+
+function setup() {
+    createCanvas(600, 600)
+    background(50)
+}
+
+function draw() {
+    background(50)
+
+    intersection.update()
+
+    stroke(255, 255, 255)
+    strokeWeight(1)
+    for (const i of intersection.lines) {
+        line(i.x1, i.y1, i.x2, i.y2)
+    }
+
+    if (mouseIsPressed) {
+        creatingLine.x2 = mouseX
+        creatingLine.y2 = mouseY
+        console.log('hi')
+    }
+
+    for (const p of intersection.currentIntersections) {
+        stroke(113, 226, 113)
+        strokeWeight(5)
+        point(p.x, p.y)
+    }
+}
+
+function mousePressed() {
+    creatingLine = intersection.createLine(mouseX, mouseY, mouseX, mouseY)
+}
